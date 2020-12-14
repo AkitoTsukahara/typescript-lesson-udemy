6014ef9c1c8867ab19dae50d58b5f8d1607433f4
AkitoTsukahara <tsukahara.akito@gmail.com>
2020-12-14 20:23:07 +0900

    lesson type-aliases

---
+++ src/type-aliases.ts
@@ -0,0 +1,21 @@
+export {};
+
+type Mojiretsu = string;
+
+const fooString: string = 'Hello';
+const fooMojiretsu: Mojiretsu = 'Hello';
+
+const example1 = {
+    name: 'Akito',
+    age: 29
+};
+
+type Profile = {
+    name: string;
+    age: number;
+};
+
+const example2: Profile = {
+    name: 'Akito',
+    age: 29
+};
