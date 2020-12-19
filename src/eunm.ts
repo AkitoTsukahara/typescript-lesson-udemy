15247864d661dff9b72667e83491876334aeb5c1
AkitoTsukahara <tsukahara.akito@gmail.com>
2020-12-20 00:05:51 +0900

    lesson enum type

---
+++ src/eunm.ts
@@ -0,0 +1,43 @@
+export {};
+
+enum Months {
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+Months.January;
+Months.February;
+
+// const MonthsJs = {
+//     January: 0,
+//     February: 1
+// }
+
+// MonthsJs.January;
+// MonthsJs.February;
+
+enum COLORS {
+    RED = '#FF0000',
+    WHITE = '#FFFFF',
+    GREEN = '#008000',
+    BLUE = '#0000FF',
+    //YELLOW = '#FFFF00',
+    BLACK = '#000000',
+}
+
+let green = COLORS.GREEN;
+
+enum COLORS {
+    YELLOW = '#FFFF00'
+}
+//COLORS.YELLOW;
