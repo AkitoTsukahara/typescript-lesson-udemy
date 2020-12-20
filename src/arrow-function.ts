114081fb9fe8b0a7a24dfb9417b3b2b534b15759
AkitoTsukahara <tsukahara.akito@gmail.com>
2020-12-20 14:20:17 +0900

    lesson arrow function

---
+++ src/arrow-function.ts
@@ -0,0 +1,13 @@
+export {};
+
+// let bmi = (height:number, weight: number): number => {
+//     return weight / (height * height)
+// }
+
+let bmi: (height:number, weight: number) => number  = (
+    height: number,
+    weight: number
+): number => weight / (height * height);
+
+
+console.log(bmi(1.7, 53));
