a86c447af1733e40e1c0e224b614f8ff1989f31d
AkitoTsukahara <tsukahara.akito@gmail.com>
2020-12-28 23:56:41 +0900

    lesson return type

--- src/exclude-extarct-nonnullable-definition.ts
+++ src/exclude-extarct-nonnullable-definition.ts
@@ -1,6 +1,11 @@
 export {};
 
-type MyExclude<T> = T extends string | number ? never : T;
+//type MyExclude<T> = T extends string | number ? never : T;
+type MyExclude<T> = 
+    | (string extends string | number ? never : string)
+    | (number extends string | number ? never : number)
+    | (DebugType extends string | number ? never : DebugType);
+
 type DebugType = () => void;
 type SomeTypes = string | number | DebugType;
 type FuctionType = Exclude<SomeTypes, string | number>;
@@ -10,3 +15,4 @@ type FunctionTypeByExtract = Extract<SomeTypes, DebugType>;
 
 type NullableTypes = string | number | null | undefined;
 type NonNullableTypes = NonNullable<NullableTypes>;
+
